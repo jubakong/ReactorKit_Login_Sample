7adb6c7e52487efe1ab92d64adfa7a45d177ecf3
jubakong <jubakong@gmail.com>
2021-06-02 10:28:18 +0900

    Add Enable IQKeyboardManger globally

--- ReactorKit-Login-Sample/SceneDelegate.swift
+++ ReactorKit-Login-Sample/SceneDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import IQKeyboardManagerSwift
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -13,6 +14,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
   func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    
+    IQKeyboardManager.shared.enable = true
+    IQKeyboardManager.shared.shouldResignOnTouchOutside = true
+    
+    
     // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
     // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
     // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
